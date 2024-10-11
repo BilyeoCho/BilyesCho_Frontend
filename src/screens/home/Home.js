aa0ad90609c1ca11720c27b1199508a04d3231a7
HeyJunN <105594922+HeyJunN@users.noreply.github.com>
2024-10-11 16:30:36 +0900

    feat: 메인페이지 컴포넌트 기본 구조 설정 #3

---
+++ src/screens/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TopBar from '../../components/TopBar';
+import Main from './Main';
+
+const Home = () => {
+  return (
+    <div>
+        <TopBar />
+        <Main />
+    </div>
+  )
+}
+
+export default Home;
+
