91e057b5df8916b21cb2e489438069e2b1e0db95
HeyJunN <105594922+HeyJunN@users.noreply.github.com>
2024-11-03 04:12:51 +0900

    style : 기존 Rent, Register / ItemRent, ItemResgister 변경

--- src/Router.js
+++ src/Router.js
@@ -3,9 +3,10 @@ import App from './App';
 import Login from "./screens/auth/Login";
 import Register from "./screens/auth/Register";
 import Home from "./screens/home/Home";
-import Rent from "./screens/rent/Rent";
-import Review from './screens/review/Review';
+import ItemRent from "./screens/rent/ItemRent";
+import ItemRegister from "./screens/register/ItemRegister";
 import Talk from './screens/talk/Talk';
+import Review from './screens/review/Review';
 import MyPage from './screens/mypage/MyPage';
 
 const router = createBrowserRouter([
@@ -26,16 +27,20 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/rent",
-        element: <Rent />,
+        path: "/itemrent",
+        element: <ItemRent />,
+      },
+      {
+        path: "/itemregister",
+        element: <ItemRegister />,
+      },
+      {
+        path: "/talk",
+        element: <Talk />,
       },
       {
         path: "/review",
         element: <Review />,
-      },
-      {
-        path: "/talk",
-        element: <Talk />
       },
       {
         path: "/mypage",
--- src/screens/register/RegisterMain.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RegisterMain = () => {
-  return (
-    <div>RegisterMain</div>
-  )
-}
-
-export default RegisterMain
