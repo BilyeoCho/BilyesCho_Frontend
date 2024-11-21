6c1f0ecd7ef080d5c07a530559d0bed857c3956c
HeyJunN <105594922+HeyJunN@users.noreply.github.com>
2024-11-21 21:38:56 +0900

    feat : 마이페이지-대여내역 퍼블리싱 #54

--- src/screens/mypage/MyPageMain.js
+++ src/screens/mypage/MyPageMain.js
@@ -2,13 +2,25 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import SideBar from '../../components/SideBar';
 import RegisterHistory from './RegisterHistory';
+import RentHistory from './RentHistory';
 
 const MyPageMain = () => {
   const [activeMenu, setActiveMenu] = useState('등록내역');
 
   const handleMenuClick = (menuName) => {
     setActiveMenu(menuName);
-    // 추후 각 메뉴별 라우팅 처리
+  };
+
+  const renderContent = () => {
+    switch (activeMenu) {
+      case '등록내역':
+        return <RegisterHistory />;
+      case '대여내역':
+        return <RentHistory />;
+      // 다른 메뉴들도 추가 예정
+      default:
+        return <RegisterHistory />;
+    }
   };
 
   return (
@@ -17,7 +29,7 @@ const MyPageMain = () => {
         activeMenu={activeMenu} 
         onMenuClick={handleMenuClick}
       />
-      <RegisterHistory />
+      {renderContent()}
     </Container>
   );
 };
